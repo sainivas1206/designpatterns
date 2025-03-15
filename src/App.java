b22e9187b1860e33e4dda47614c104728c5fa357
sainivas <sainivasthanniru@gmail.com>
2025-03-15 16:39:00 +0530

    Updated .gitignore

--- src/App.java
+++ src/App.java
@@ -1,13 +1,6 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-
 public class App {
 
     public static void main(String[] args) {
-        List<String> title = Arrays.asList("Modern", "Java", "In", "Action");
-        Stream<String> s = title.stream();
-        s.forEach(System.out::println);
-        s.forEach(System.out::println);
+
     }
 }
